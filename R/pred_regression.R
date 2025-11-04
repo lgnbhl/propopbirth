a652271d0fdd134bf80f06d1a4d7d09e60d0882e
sszrok <klemens.rosin@zuerich.ch>
2025-11-04 09:32:07 +0100

    pred_regression: @return

--- R/pred_regression.R
+++ R/pred_regression.R
@@ -9,7 +9,7 @@
 #' @param age_min minimum fertile age
 #' @param age_max maximum fertile age
 #'
-#' @return
+#' @return tibble with prediction data per age year
 #'
 #' @noRd
 pred_regression <- function(y, x1, x2, x3, x4, x5, age_min, age_max) {
